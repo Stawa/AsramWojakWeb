a79bb86c3d9b9e5d2f63ffeea00be9594fded556
Stawa <stawamusic@gmail.com>
2024-08-18 14:03:21 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
             <div className="flex justify-center space-x-4">
               <a
                 href="#server-ips"
-                className="inline-block px-6 py-3 text-white bg-blue-500 hover:bg-blue-600 rounded-md font-semibold transition duration-300"
+                className="inline-block px-6 py-3 bg-white text-black hover:bg-neutral-300 rounded-md font-semibold transition duration-300"
               >
                 Join Now!
               </a>
               <a
                 href="#features"
-                className="inline-block px-6 py-3 text-blue-500 border border-blue-500 hover:bg-blue-100 rounded-md font-semibold transition duration-300"
+                className="inline-block px-6 py-3 bg-neutral-900 text-white border border-neutral-500 hover:bg-neutral-800 rounded-md font-semibold transition duration-300"
               >
                 Features
               </a>
@@ -45,84 +45,74 @@ export default function Home() {
             <p className="text-lg md:text-xl mx-auto mt-4 max-w-2xl">
               Connect to Asram Wojak&apos;s server and join the adventure!
               <br />
-              Connect to our server now on Java or Bedrock
+              Connect to our server now on Java or Bedrock.
             </p>
           </div>
 
           <div className="flex flex-col md:flex-row justify-center gap-6">
-            <div className="relative overflow-hidden rounded-lg border border-neutral-600 bg-neutral-950 p-4 shadow-lg flex-1 min-w-[300px] max-w-[500px]">
-              <div className="flex flex-col h-full justify-between rounded-md p-4">
-                <div className="flex justify-center mb-4 w-full">
-                  <Image
-                    src="/Java_Banner.webp"
-                    alt="Minecraft Java Banner"
-                    width={400}
-                    height={64}
-                    className="object-cover rounded-lg"
-                  />
-                </div>
-                <div className="flex flex-col items-start w-full">
-                  <div className="flex items-center mb-4 w-full">
-                    <Image
-                      src="/Java_Edition.webp"
-                      alt="Minecraft Java"
-                      width={48}
-                      height={48}
-                      className="mr-4"
-                    />
-                    <h3 className="font-bold text-xl text-white">
-                      Minecraft Java
-                    </h3>
-                  </div>
-                  <div className="space-y-2 text-left">
-                    <p className="text-sm font-bold text-gray-300">
-                      Server IP:
-                    </p>
-                    <p className="text-lg font-medium text-gray-200">
-                      play.asram-wojak.my.id
-                    </p>
-                    <p className="text-sm font-bold text-gray-300">Port:</p>
-                    <p className="text-lg font-medium text-gray-200">-</p>
-                  </div>
-                </div>
+            {/* Minecraft Java */}
+            <div className="border border-neutral-600 bg-neutral-950 rounded-lg shadow-lg p-6 flex-1 w-full md:w-1/3 lg:w-1/4">
+              <div className="flex justify-center mb-4">
+                <Image
+                  src="/Java_Banner.webp"
+                  alt="Minecraft Java Banner"
+                  width={500}
+                  height={100}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+              <div className="flex items-center mb-4">
+                <Image
+                  src="/Java_Edition.webp"
+                  alt="Minecraft Java"
+                  width={48}
+                  height={48}
+                  className="mr-4"
+                />
+                <h3 className="font-bold text-2xl text-white">
+                  Minecraft Java
+                </h3>
+              </div>
+              <div className="space-y-2">
+                <p className="text-sm font-bold text-gray-300">Server IP:</p>
+                <p className="text-lg font-medium text-gray-200">
+                  play.asram-wojak.my.id
+                </p>
+                <p className="text-sm font-bold text-gray-300">Port:</p>
+                <p className="text-lg font-medium text-gray-200">-</p>
               </div>
             </div>
 
-            <div className="relative overflow-hidden rounded-lg border border-neutral-600 bg-neutral-950 p-4 shadow-lg flex-1 min-w-[300px] max-w-[500px]">
-              <div className="flex flex-col h-full justify-between rounded-md p-4">
-                <div className="flex justify-center mb-4 w-full">
-                  <Image
-                    src="/Bedrock_Banner.webp"
-                    alt="Minecraft Bedrock Banner"
-                    width={400}
-                    height={64}
-                    className="object-cover rounded-lg"
-                  />
-                </div>
-                <div className="flex flex-col items-start w-full">
-                  <div className="flex items-center mb-4 w-full">
-                    <Image
-                      src="/Bedrock_Edition.webp"
-                      alt="Minecraft Bedrock"
-                      width={48}
-                      height={48}
-                      className="mr-4"
-                    />
-                    <h3 className="font-bold text-xl text-white">
-                      Minecraft Bedrock
-                    </h3>
-                  </div>
-                  <div className="space-y-2 text-left">
-                    <p className="text-sm font-bold text-gray-300">
-                      Server IP:
-                    </p>
-                    <p className="text-lg font-medium text-gray-200">
-                      mc.asram-wojak.my.id
-                    </p>
-                    <p className="text-sm font-bold text-gray-300">Port:</p>
-                    <p className="text-lg font-medium text-gray-200">33669</p>
-                  </div>
-                </div>
+            {/* Minecraft Bedrock */}
+            <div className="border border-neutral-600 bg-neutral-950 rounded-lg shadow-lg p-6 flex-1 w-full md:w-1/3 lg:w-1/4">
+              <div className="flex justify-center mb-4">
+                <Image
+                  src="/Bedrock_Banner.webp"
+                  alt="Minecraft Bedrock Banner"
+                  width={500}
+                  height={100}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+              <div className="flex items-center mb-4">
+                <Image
+                  src="/Bedrock_Edition.webp"
+                  alt="Minecraft Bedrock"
+                  width={48}
+                  height={48}
+                  className="mr-4"
+                />
+                <h3 className="font-bold text-2xl text-white">
+                  Minecraft Bedrock
+                </h3>
+              </div>
+              <div className="space-y-2">
+                <p className="text-sm font-bold text-gray-300">Server IP:</p>
+                <p className="text-lg font-medium text-gray-200">
+                  mc.asram-wojak.my.id
+                </p>
+                <p className="text-sm font-bold text-gray-300">Port:</p>
+                <p className="text-lg font-medium text-gray-200">33669</p>
               </div>
             </div>
           </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,36 @@
+"use client";
 import React from "react";
 
 const Footer: React.FC = () => {
+  const handleClick = (message: string) => {
+    alert(message);
+  };
+
   return (
     <footer className="border-t border-sky-500 text-white py-6">
       <div className="container mx-auto px-6 text-center">
         <p className="text-md mb-2">
-          &copy; {new Date().getFullYear()} Stawa. All rights
-          reserved.
+          &copy; {new Date().getFullYear()} Stawa. All rights reserved.
         </p>
         <div className="flex justify-center space-x-4">
-          <a className="text-gray-400 hover:text-white">Privacy Policy</a>
-          <a className="text-gray-400 hover:text-white">Terms of Service</a>
-          <a className="text-gray-400 hover:text-white">Contact Us</a>
+          <a
+            className="text-gray-400 hover:text-white"
+            onClick={() => handleClick("Privacy Policy is not added yet!")}
+          >
+            Privacy Policy
+          </a>
+          <a
+            className="text-gray-400 hover:text-white"
+            onClick={() => handleClick("Terms of Service is not added yet!")}
+          >
+            Terms of Service
+          </a>
+          <a
+            className="text-gray-400 hover:text-white"
+            onClick={() => handleClick("Contact Us page is not added yet!")}
+          >
+            Contact Us
+          </a>
         </div>
       </div>
     </footer>
